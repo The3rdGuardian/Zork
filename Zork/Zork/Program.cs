9f79a7282b06670c189c7c8d186945fe61c29db7
E <edwaddpowers3@knights.ucf.edu>
2020-09-02 13:41:56 -0400

    Zork 1.2 - Finding your Direction

--- Zork/Zork/Program.cs
+++ Zork/Zork/Program.cs
@@ -9,25 +9,13 @@ namespace Zork
             Console.WriteLine("Welcome to Zork!");
 
             string inputString = Console.ReadLine();
+            Commands command = ToCommand(inputString.Trim().ToUpper());
+            Console.WriteLine(command);
 
-            inputString = inputString.ToUpper();
-
-            //Zork 1.1 - Looking around and quitting
-            if(inputString == "QUIT")
-            {
-                Console.WriteLine("Thank you for playing!");
-            }
-            else if(inputString == "LOOK")
-            {
-
-                Console.WriteLine("This is an open field west of a white house, with a boarded front door. \nA rubber mat saying 'Welcome to Zork' lies by the door.");
-
-            }
-            else
-            {
-                Console.WriteLine("Unrecognized command");
-            }
             
         }
+
+        static Commands ToCommand(string inputString) => Enum.TryParse(inputString, true, out Commands result) ? result : Commands.UNKNOWN;
+
     }
 }
